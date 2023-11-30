a2ed8559f8d56ab771baddf7542587faf6f3400a
Jared Williams <jxwilliams11@ualr.edu>
2023-11-29 18:56:37 -0600

    adding landing page
    -removed header from index.html and moved to layout
    -added landing page
    -layout is wrapped around each component in index.js
    -in index.js, user begins at landing page.   header redirects to layout.
    -in index.js, once user is logged in, header redirects to home page.
    -added style page for components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import Login from './components/loginComponent.js'
+import Login from './components/loginComponent.js';
 import SignUp from './components/signupComponent.js';
-import Home from './components/homeComponent.js'
-import Layout from './components/layoutComponent.js'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-
+import Home from './components/homeComponent.js';
+import Layout from './components/layoutComponent.js';
+import Landing from './components/landingComponent.js'; // Import the landing page component
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -19,6 +19,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route
+<<<<<<< Updated upstream
           path="/login"
           element={
             isAuthenticated ? <Navigate to="/" replace /> : <Login onLogin={handleLogin} />
@@ -26,6 +27,8 @@ function App() {
         />
         <Route path="/signup" element={<SignUp />} />
         <Route
+=======
+>>>>>>> Stashed changes
           path="/"
           element={
             isAuthenticated ? (
@@ -33,8 +36,36 @@ function App() {
                   <Home />
               </Layout>
             ) : (
+<<<<<<< Updated upstream
                 <Navigate to="/login" replace />
+=======
+              <Navigate to="/landing" /> 
+>>>>>>> Stashed changes
             )
+          }
+        />
+         <Route
+          path="/landing"
+          element={
+            <Layout>
+              <Landing />
+            </Layout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Layout>
+              <SignUp />
+            </Layout>
           }
         />
       </Routes>
