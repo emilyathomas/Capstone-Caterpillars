d25ad9e7d2edb59c7070c7510e5448c07ac06046
JuanPerez1234567 <111318836+Juan-Alverto-Perez@users.noreply.github.com>
2023-12-08 01:12:53 -0600

    search functionality added

--- api/routes/home.js
+++ api/routes/home.js
@@ -1,6 +1,8 @@
+
 var express = require('express');
 const router = express.Router();
 const Employer = require('../models/Employer.js');
+const { Op , literal} = require('sequelize');
 
 const sampleData = [
     { id: 1, companyName: 'best buy', headquartersAddress: 'a;lsfkj@lgfks', descendantCompanies: "13123123", predecessorCompanies: '', hasEmployed: 20 },
@@ -18,5 +20,22 @@ router.get("/", async (req, res) => {
         return res.status(500).json({ success: false, message: 'Internal server error' });
     }
 });
+
+router.post("/search", async (req, res) =>{
+    const {query} = req.body;
+    try {
+      const results = await Employer.findAll({
+        where: {
+          companyName: {
+            [Op.like]: literal(`'%${query}%' COLLATE utf8mb4_general_ci`),
+          },
+        },
+      })
+      res.json(results);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ success: false, message: 'Insafdternal server error' });
+    }
+  });
 
 module.exports = router;
--- frontend/src/components/homeComponent.js
+++ frontend/src/components/homeComponent.js
@@ -6,6 +6,7 @@ import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
+import Button from '@mui/material/Button'
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
@@ -20,46 +21,35 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
   }));
 
-const Search = styled('div')(({ theme }) => ({
+  const StyledSearch = styled('div')(({ theme }) => ({
     position: 'relative',
-    borderRadius: 20,
-    backgroundColor: alpha(theme.palette.common.white, 1),
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: theme.palette.grey[200],
     '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 1),
+      backgroundColor: theme.palette.grey[300],
     },
-    margin:theme.spacing(2),
+    marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
+      marginLeft: theme.spacing(1),
       width: 'auto',
     },
   }));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
     '& .MuiInputBase-input': {
       padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
       paddingLeft: `calc(1em + ${theme.spacing(4)})`,
       transition: theme.transitions.create('width'),
-      width: '100%',
+      width: '50%',
       [theme.breakpoints.up('md')]: {
         width: '20ch',
       },
     },
   }));
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-
   function Home() {
-    const [data, setData] = useState([]);
+    const [cards, setCards] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -68,7 +58,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
                 const response = await fetch("http://localhost:4000/home");
                 console.log(response)
                 const result = await response.json();
-                setData(result);
+                setCards(result);
             } catch (error) {
                 console.error('Error fetching Employer Data:');
                 console.log(error);
@@ -80,11 +70,11 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 
       const renderCards = () => {
-          if (!data || data.length === 0) {
+          if (! cards || cards.length === 0) {
               console.log("Unable to load employer cards.")
               return <Typography>No data available</Typography>;
           }
-          return data.map((item) => (
+          return cards.map((item) => (
               <Grid item xs={4} padding={'20px'}>
                   <Card key={item.id}>
                       <CardActionArea>
@@ -98,33 +88,80 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
               </Grid>
           ));
       };
+      const [query, setQuery] = useState('');
+      
+      const clearCards = () => {
+        setCards([]);
+      };
+      const handleSearch = async () => {
+        try {
+          console.log(query);
+          const response = await fetch('http://localhost:4000/home/search', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ query }),
+            
+          });
+
+          const cards = await response.json();
+          console.log(cards)
+          clearCards();
+          setCards(cards);
+          renderCards();
+
+        } catch (error) {
+          console.error('Error fetching search results:', error);
+        }
+      };
+
+      const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+          handleSearch();
+        }
+      };
+
+      
+
+      
+
     return (
         <Box>
-
-            <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-            </Search>
-            <Grid container spacing={2} backgroundColor="white" margin={'20 px'} padding={'10px'} boxShadow={20}>
-                <Grid item xs={2} justifyContent={'center'} alignContent={'center'} boxShadow={30} style={{ display: 'flex' }}>
-                    <Stack spacing={2} useFlexGap justifyContent={'center'} alignContent={'center'} width={'auto'}>
-                        <Item>Filter 1</Item>
-                        <Item>Filter 2</Item>
-                        <Item>Filter 3</Item>
-                    </Stack>
+            <Grid container  justifyContent={"center"}>
+              <Grid item >
+                <StyledSearch width={"50%"} border={"10px solid black"}>
+                    <StyledInputBase width={"50%"} border={"10px solid black"}
+                      placeholder="Search…"
+                      inputProps={{ 'aria-label': 'search' }}
+                      value={query}
+                      onChange={(e) => setQuery(e.target.value)}
+                      onKeyPress={handleKeyPress}
+                    />
+                    <Button variant="contained" onClick={handleSearch} endIcon={<SearchIcon />}>
+                      Search
+                    </Button>
+                  </StyledSearch>
                 </Grid>
-                <Grid item xs={10} padding={'20px'}>
-                  <Grid container spacing={2} backgroundColor="#D3D3D3" padding={'20px'} border={"10px solid white"}>
+              </Grid>
+            
+            <Grid container spacing={2} backgroundColor="white" margin={'20 px'} padding={'10px'} boxShadow={20}>
+              
+              <Grid item xs={2} justifyContent={'center'} alignContent={'center'} boxShadow={30} style={{ display: 'flex' }}>
+                  <Stack spacing={2} useFlexGap justifyContent={'center'} alignContent={'center'} width={'auto'}>
+                      <Item>Filter 1</Item>
+                      <Item>Filter 2</Item>
+                      <Item>Filter 3</Item>
+                  </Stack>
+              </Grid>
+              <Grid item xs={10} padding={'20px'}>
+                <Grid container spacing={2} backgroundColor="#D3D3D3" padding={'20px'} border={"10px solid white"}>
                     
                   {renderCards()}
                     
-                  </Grid></Grid>
-                </Grid>     
+                </Grid>
+              </Grid>
+            </Grid>     
 
 
         
