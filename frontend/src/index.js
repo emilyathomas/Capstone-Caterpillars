bc68780c9a9b27d395f823fbfa0d3f629eaac211
Ky Stewart <kylota@yahoo.com>
2023-12-08 01:54:40 -0600

    replaced linking to landingpage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,7 +28,7 @@ function App() {
         <Route 
           path="/"
           element={
-            <Navigate to="/home" replace/>
+            <Navigate to="/landing" replace/>
           }
         />
         <Route
