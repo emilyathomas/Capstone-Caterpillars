783d2241226db8adc2655134ec9a8176c38cbf9c
JuanPerez1234567 <111318836+Juan-Alverto-Perez@users.noreply.github.com>
2023-12-12 00:47:26 -0600

    added edit and delete functionality

--- api/routes/home.js
+++ api/routes/home.js
@@ -32,5 +32,41 @@ router.post("/search", async (req, res) => {
     return res.status(500).json({ success: false, message: 'Internal server error' });
   }
 });
+router.post("/edit/:employerID", async (req, res) => {
+  const employerID = req.params.employerID;
+  const updatedData = req.body;
+
+  try {
+    const employer = await Employer.findByPk(employerID);
+
+    if (!employer) {
+      return res.status(404).json({ message: "Employer not found" });
+    }
+
+    await employer.update(updatedData);
+
+    res.status(200).json({ message: "Employer updated successfully" });
+  } catch (error) {
+    console.error("Error updating employer:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+router.delete('/delete/:employerID', async (req, res) => {
+  const { employerID } = req.params;
+
+  try {
+    const employer = await Employer.findByPk(employerID);
+
+    if (!employer) {
+      return res.status(404).json({ error: 'Employer not found' });
+    }
+
+    await employer.destroy();
+    return res.status(200).json({ message: 'Employer deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting employer:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 module.exports = router;
--- frontend/src/components/homeComponent.js
+++ frontend/src/components/homeComponent.js
@@ -1,56 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import TreeDiagram from './digraphComponent.js';
-import { useNavigate } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import SearchIcon from '@mui/icons-material/Search';
-import { styled } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import Button from '@mui/material/Button';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import Stack from '@mui/material/Stack';
-import AddEmployer from '../components/addemployerComponent.js';
-
+import React, { useEffect, useState } from "react";
+import TreeDiagram from "./digraphComponent.js";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import AddEmployer from "../components/addemployerComponent.js";
+import EditIcon from "@mui/icons-material/Edit";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import TextField from "@mui/material/TextField";
+import CardActions from "@mui/material/CardActions";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   padding: theme.spacing(1),
-  margin: '0 auto',
-  textAlign: 'center',
-  width: '100%',
+  margin: "0 auto",
+  textAlign: "center",
+  width: "100%",
   color: theme.palette.text.secondary,
-  position: 'sticky',
+  position: "sticky",
   top: 0,
 }));
 
-const StyledSearch = styled('div')(({ theme }) => ({
-  position: 'relative',
-  display: 'flex',
+const StyledSearch = styled("div")(({ theme }) => ({
+  position: "relative",
+  display: "flex",
   padding: theme.spacing(0, 0),
-  margin: '0 auto',
+  margin: "0 auto",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: theme.palette.grey[200],
-  '&:hover': {
+  "&:hover": {
     backgroundColor: theme.palette.grey[300],
   },
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(2)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('md')]: {
-      width: 'auto',
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("md")]: {
+      width: "auto",
     },
   },
 }));
@@ -61,6 +70,17 @@ function Home({ onLogout }) {
   const navigate = useNavigate();
   const [query, setQuery] = useState('');
   const [showAddEmployerForm, setShowAddEmployerForm] = useState(false);
+  const [editDialogOpen, setEditDialogOpen] = useState(false);
+  const [editData, setEditData] = useState({
+    companyName: "",
+    headquartersAddress: "",
+    hasEmployed: "",
+  });
+  const [editingEmployerID, setEditingEmployerID] = useState(null);
+  const [isMenuOpen, setIsMenuOpen] = useState({});
+  const [hoveredCard, setHoveredCard] = useState(null);
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [deletingEmployerID, setDeletingEmployerID] = useState(null);
 
   const clearCards = () => {
     setCards([]);
@@ -69,46 +89,42 @@ function Home({ onLogout }) {
   const handleSearch = async () => {
     try {
       console.log(query);
-      const response = await fetch('http://localhost:4000/home/search', {
-        method: 'POST',
+      const response = await fetch("http://localhost:4000/home/search", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ query }),
-
       });
 
       const cards = await response.json();
-      console.log(cards)
+      console.log(cards);
       clearCards();
       setCards(cards);
       renderCards();
-
     } catch (error) {
-      console.error('Error fetching search results:', error);
+      console.error("Error fetching search results:", error);
     }
   };
 
   const handleButtonClick = () => {
     if (showAddEmployerForm) {
       setShowAddEmployerForm(false);
-
+    } else {
+      setShowAddEmployerForm("addEmployer");
     }
-    else {
-      setShowAddEmployerForm('addEmployer');
-    }
-    console.log('Add Employer Button Clicked');
+    console.log("Add Employer Button Clicked");
   };
 
   const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       handleSearch();
     }
   };
 
   const handleLogout = () => {
     onLogout();
-    navigate('/landing');
+    navigate("/landing");
   };
 
   const handleCardClick = (employeeID) => {
@@ -117,20 +133,78 @@ function Home({ onLogout }) {
     navigate(`/tree/${employeeID}`, { state: { showTree: true } });
   };
 
+  const handleEdit = (employerID, event) => {
+    event.stopPropagation();
+    console.log("Editing " + employerID);
+    
+
+    setEditingEmployerID(employerID);
+    setEditDialogOpen(true);
+  };
+  const handleEditDialogClose = () => {
+    setEditDialogOpen(false);
+    setEditingEmployerID(null);
+  };
+  const handleMenuToggle = (event, employerID) => {
+    event.stopPropagation();
+    setHoveredCard(hoveredCard === employerID ? null : employerID);
+
+    setIsMenuOpen((prevMenuState) => ({
+      ...prevMenuState,
+      [employerID]: !prevMenuState[employerID],
+    }));
+  };
+  const handleDeleteDialog = (employerID, event) => {
+    event.stopPropagation();
+    setDeletingEmployerID(employerID);
+    setDeleteDialogOpen(true);
+  };
+  const handleConfirmDelete = async (employerID) => {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/home/delete/${employerID}`,
+        {
+          method: "DELETE",
+        }
+      );
+      console.log(employerID)
+
+      if (response.ok) {
+        console.log("Employer deleted successfully");
+        setDeleteDialogOpen(false);
+        window.location.reload();
+      } else {
+        console.error("Error deleting employer:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error deleting employer:", error);
+    }
+  };
+
   const renderCards = () => {
     if (!cards || cards.length === 0) {
-      console.log("Unable to load employer cards.")
+      console.log("Unable to load employer cards.");
       return <Typography>No data available</Typography>;
     }
     return cards.map((item) => (
-      <Grid item xs={12} sm={6} md={4} lg={3} key={item.employerID} style={{ padding: '10px' }}>
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        md={4}
+        lg={4}
+        key={item.employerID}
+        style={{ padding: "10px", position: "relative" }}
+        onMouseEnter={() => setHoveredCard(item.employerID)}
+        onMouseLeave={() => setHoveredCard(null)}
+      >
         <Card
           key={item.employerID}
           onClick={() => {
             console.log("Employee ID:", item.employerID);
             handleCardClick(item.employerID); // Pass item.employerID here
           }}
-          style={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+          style={{ height: "100%", display: "flex", flexDirection: "column" }}
         >
           <CardActionArea>
             <CardContent>
@@ -139,6 +213,44 @@ function Home({ onLogout }) {
               <Typography>{item.hasEmployed}</Typography>
             </CardContent>
           </CardActionArea>
+
+          {hoveredCard === item.employerID && (
+            <div
+              style={{
+                justifyContent: "flex-start",
+                alignContent: "flex-start",
+                position: "absolute",
+                right: "0",
+              }}
+            >
+              <IconButton
+                style={{ justifyContent: "flex-end", color: "#035AA6" }}
+                onClick={(e) => handleMenuToggle(e, item.employerID)}
+              >
+                <MoreVertIcon />
+              </IconButton>
+            </div>
+          )}
+
+          {isMenuOpen[item.employerID] && (
+            <Card style={{ backgroundColor: "#D3D3D3", margin: "5px"}}>
+              <CardActions>
+                <IconButton
+                  style={{ color: "#035AA6" }}
+                  onClick={(e) => handleEdit(item.employerID, e)}
+                >
+                  <EditIcon />
+                </IconButton>
+
+                <IconButton
+                  style={{ color: "red" }}
+                  onClick={(e) => handleDeleteDialog(item.employerID, e)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          )}
         </Card>
       </Grid>
     ));
@@ -147,14 +259,14 @@ function Home({ onLogout }) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.log('Fetching Employer Data')
+        console.log("Fetching Employer Data");
         const response = await fetch("http://localhost:4000/home");
-        console.log(response)
+        console.log(response);
         const result = await response.json();
         setCards(result);
-        console.log(result)
+        console.log(result);
       } catch (error) {
-        console.error('Error fetching Employer Data:');
+        console.error("Error fetching Employer Data:");
         console.log(error);
       }
     };
@@ -170,69 +282,200 @@ function Home({ onLogout }) {
         setShowTree(false);
       }
     };
-    window.addEventListener('popstate', handlePopState);
+    window.addEventListener("popstate", handlePopState);
     return () => {
-      window.removeEventListener('popstate', handlePopState);
+      window.removeEventListener("popstate", handlePopState);
     };
   }, []);
+
+  const handleEditSubmit = async (employerID) => {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/home/edit/${employerID}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(editData),
+        }
+      );
+
+      if (response.ok) {
+        setEditDialogOpen(false);
+        setEditingEmployerID(null);
+        window.location.reload();
+      } else {
+        console.error("Error updating employer:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error updating employer:", error);
+    }
+  };
 
   return (
     <Box>
       {showTree ? (
         <TreeDiagram />
-      ) :
-        (
-          <>
-            <Grid container spacing={2} backgroundColor="white" margin={'auto'} padding={'20px'} boxShadow={20}>
-              <Grid container alignItems="center" justifyContent="space-between" spacing={2} style={{ marginTop: '0px' }}>
-                <Grid>
-                  {/*Search*/}
-                  <Grid style={{ marginTop: '0px' }}>
-                    <Grid container justifyContent="flex-start">
-                      <StyledSearch width={"100vw"} border={"10px solid black"} display={"flex"} justifyContent={"flex-end"}>
-                        <StyledInputBase width={"100vw"} display={"flex"} border={"10px solid black"}
-                          placeholder="Search…"
-                          inputProps={{ 'aria-label': 'search' }}
-                          value={query}
-                          onChange={(e) => setQuery(e.target.value)}
-                          onKeyPress={handleKeyPress}
-                        />
-                      </StyledSearch>
-                      <Button variant="contained" onClick={handleSearch} endIcon={<SearchIcon />}>
-                        Search
-                      </Button>
-                    </Grid>
+      ) : (
+        <>
+          <Dialog open={editDialogOpen} onClose={handleEditDialogClose}>
+            <DialogTitle>Edit Employer</DialogTitle>
+            <DialogContent>
+              <TextField
+                label="Company Name"
+                value={editData.companyName}
+                onChange={(e) =>
+                  setEditData({ ...editData, companyName: e.target.value })
+                }
+                fullWidth
+              />
+              <TextField
+                label="Headquarters Address"
+                value={editData.headquartersAddress}
+                onChange={(e) =>
+                  setEditData({
+                    ...editData,
+                    headquartersAddress: e.target.value,
+                  })
+                }
+                fullWidth
+              />
+              <TextField
+                label="Has Employed"
+                value={editData.hasEmployed}
+                onChange={(e) =>
+                  setEditData({ ...editData, hasEmployed: e.target.value })
+                }
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleEditDialogClose}>Cancel</Button>
+              <Button onClick={() => handleEditSubmit(editingEmployerID)}>
+                Save
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Dialog
+            open={deleteDialogOpen}
+            onClose={() => setDeleteDialogOpen(false)}
+          >
+            <DialogTitle>Delete Employer</DialogTitle>
+            <DialogContent>
+              <Typography>
+                Are you sure you want to delete this employer?
+              </Typography>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => setDeleteDialogOpen(false)}>Cancel</Button>
+              <Button onClick={() => handleConfirmDelete(deletingEmployerID)}>
+                Confirm Delete
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          <Grid
+            container
+            spacing={2}
+            backgroundColor="white"
+            margin={"auto"}
+            padding={"20px"}
+            boxShadow={20}
+          >
+            <Grid
+              container
+              alignItems="center"
+              justifyContent="space-between"
+              spacing={2}
+              style={{ marginTop: "0px" }}
+            >
+              <Grid>
+                {/*Search*/}
+                <Grid style={{ marginTop: "0px" }}>
+                  <Grid container justifyContent="flex-start">
+                    <StyledSearch
+                      width={"100vw"}
+                      border={"10px solid black"}
+                      display={"flex"}
+                      justifyContent={"flex-end"}
+                    >
+                      <StyledInputBase
+                        width={"100vw"}
+                        display={"flex"}
+                        border={"10px solid black"}
+                        placeholder="Search…"
+                        inputProps={{ "aria-label": "search" }}
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                        onKeyPress={handleKeyPress}
+                      />
+                    </StyledSearch>
+                    <Button
+                      variant="contained"
+                      onClick={handleSearch}
+                      endIcon={<SearchIcon />}
+                    >
+                      Search
+                    </Button>
                   </Grid>
                 </Grid>
-                <Button onClick={handleLogout} variant="contained" color="primary">
-                  Logout
-                </Button>
               </Grid>
-              {/* Filters */}
-              <Grid item xs={2} justifyContent={'center'} alignContent={'center'} boxShadow={30} style={{ display: 'sticky' }}>
-                <Stack spacing={2} useFlexGap justifyContent={'center'} alignContent={'center'}>
-                  <Item>Filter 1</Item>
-                  <Item>Filter 2</Item>
-                  <Item>Filter 3</Item>
-                  <Item>
-                    <div>
-                      <button onClick={handleButtonClick}>Add Employer</button>
-                      {showAddEmployerForm ? <AddEmployer /> : null}
-                    </div>
-                  </Item>
-                </Stack>
-              </Grid>
-              {/* Cards */}
-              <Grid item xs={10} style={{ padding: '20px' }}>
-                <Grid container spacing={1} style={{ backgroundColor: "#D3D3D3", padding: 'px', border: "10px solid white", height: '100%', boxSizing: 'border-box' }}>
-                  {renderCards()}
-                </Grid>
+              <Button
+                onClick={handleLogout}
+                variant="contained"
+                color="primary"
+              >
+                Logout
+              </Button>
+            </Grid>
+            {/* Filters */}
+            <Grid
+              item
+              xs={2}
+              justifyContent={"center"}
+              alignContent={"center"}
+              boxShadow={30}
+              style={{ display: "sticky" }}
+            >
+              <Stack
+                spacing={2}
+                useFlexGap
+                justifyContent={"center"}
+                alignContent={"center"}
+              >
+                <Item>Filter 1</Item>
+                <Item>Filter 2</Item>
+                <Item>Filter 3</Item>
+                <Item>
+                  <div>
+                    <button onClick={handleButtonClick}>Add Employer</button>
+                    {showAddEmployerForm ? <AddEmployer /> : null}
+                  </div>
+                </Item>
+              </Stack>
+            </Grid>
+            {/* Cards */}
+            <Grid item xs={10} style={{ padding: "20px" }}>
+              <Grid
+                container
+                spacing={1}
+                style={{
+                  backgroundColor: "#D3D3D3",
+                  padding: "px",
+                  border: "10px solid white",
+                  height: "100%",
+                  boxSizing: "border-box",
+                }}
+              >
+                {renderCards()}
               </Grid>
             </Grid>
-          </>
-        )}
+          </Grid>
+        </>
+      )}
     </Box>
   );
 }
 
-export default Home
+export default Home;
